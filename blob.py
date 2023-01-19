234ff3f4b6437ee3df67ebf13cddd1856c2ba30a
Robby Divine <rdivine48@gmail.com>
2023-01-18 21:58:29 -0800

    adding functions to save or align all, cleaning things up

--- blob.py
+++ blob.py
@@ -17,17 +17,10 @@ def blob(designs):
 	cmd.do("alter all, q=1")
 	cmd.do("set gaussian_resolution,15")
 
-	designs = cmd.get_object_list(designs)
-	for design_name in designs:
-		print(design_name)
+	for design_name in cmd.get_object_list(designs):
 		for chain_id in cmd.get_chains(design_name):
 			#cmd.do("create copy_" + chain_id + ", " + design_name + " and chain " + chain_id)
 			cmd.do("map_new map_{chain_id}_{design_name}, gaussian, 1, ({design_name} and chain {chain_id} and name CA), 10".format(chain_id=chain_id, design_name=design_name))
 			cmd.do("isosurface blob_{chain_id}_{design_name}, map_{chain_id}_{design_name}".format(chain_id=chain_id, design_name=design_name))
 
-
-
-
 cmd.extend( "blob", blob )
-
-
--- util.py
+++ util.py
@@ -1,15 +1,27 @@
 from pymol import cmd
 import glob
 
-def ld(pdb_name=all):
-	pdbs = glob.glob(pdb_name)
-	print(pdbs)
-	for i in pdbs:
-		if i[-3:] == 'pdb':
-			cmd.do('load {i}'.format(i=i))
-		elif i[-3:] == '.gz':
+def ld(pdb_name='all'):
+	for i in glob.glob(pdb_name):
+		if i[-3:] in ('pdb', '.gz'):
 			cmd.do('load {i}'.format(i=i))
 
+def align_all(obj_to_align='all'):
+    all_obj = cmd.get_object_list(obj_to_align)
 
+    if len(all_obj) < 2:
+        return 'You must provide at least two objects to align!'
+    
+    target = all_obj[0]
+    for i in all_obj[1:]:
+        cmd.do('align {i}, {target}'.format(i=i, target=target))
 
-cmd.extend( "ld", ld)
+def save_all(save_path, obj_to_save='all'):
+    if save_path[-1] != '/':
+        save_path += '/'
+    for i in cmd.get_object_list(obj_to_save):
+        cmd.do('save {save_path}{i}.pdb, {i}'.format(save_path=save_path, i=i))
+
+cmd.extend('ld', ld)
+cmd.extend('align_all', align_all)
+cmd.extend('save_all', save_all)
